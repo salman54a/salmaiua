c13e3a3ccefa1a535400fbe49575d10f59f65ef9
salman54a <runidorbesh@gmail.com>
2024-08-28 03:30:08 +0000

    feat: initiated the project

---
+++ contracts/Token.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract TestToken is ERC20 {
+    constructor()ERC20("693911e1aa0b32d531a6a0507d7d219fcfa2f43545d56ae7b8b8065166192ebe",""){} 
+
+    function mint100tokens() public {
+        _mint(msg.sender, 100*10**18);
+    }
+
+    function burn100tokens() public{
+        _burn(msg.sender, 100*10**18);
+    }
+}
